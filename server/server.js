6f93539139b0ca1dd4ab005589147737d2396195
Josh Gough <jsgough@gmail.com>
2014-08-30 21:26:41 +0000

    Upgraded to 0.9

--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
-  Meteor.startup(function () {
+Meteor.startup(function () {
     if (Levels.find().count() === 0) {
       Levels.insert({name: "Level 1"});
     }
     API = new CollectionAPI({});
     API.addCollection(Levels, 'levels');
     API.start();    
-  });
+});
