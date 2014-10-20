9d7f2bfa14a4c4aaeb563b2ba41358383187862c
joshgough <josh.gough@versionone.com>
2014-10-20 03:40:28 +0000

    Reload level assets on each play click (avoid cache)

--- client/views/home/home.js
+++ client/views/home/home.js
@@ -146,13 +146,110 @@ _.extend(Template.home, {
       Q.state.reset({ score: 0, ammo: 0, lives: 2, stage: 1});
 
       Q.gravityX = 0;
-      Q.gravityY = 0;
-  
+      Q.gravityY = 0;  
+    
+      // Override quintus load
+      Q.load = function(assets,callback,options) {
+          var assetObj = {};
+
+          /* Make sure we have an options hash to work with */
+          if(!options) { options = {}; }
+
+          /* Get our progressCallback if we have one */
+          var progressCallback = options.progressCallback;
+
+          var errors = false,
+              errorCallback = function(itm) {
+                errors = true;
+                (options.errorCallback  ||
+                 function(itm) { throw("Error Loading: " + itm ); })(itm);
+              };
+
+          /* Convert to an array if it's a string */
+          if(Q._isString(assets)) {
+            assets = Q._normalizeArg(assets);
+          }
+
+          /* If the user passed in an array, convert it */
+          /* to a hash with lookups by filename */
+          if(Q._isArray(assets)) {
+            Q._each(assets,function(itm) {
+              if(Q._isObject(itm)) {
+                Q._extend(assetObj,itm);
+              } else {
+                assetObj[itm] = itm;
+              }
+            });
+          } else {
+            /* Otherwise just use the assets as is */
+            assetObj = assets;
+          }
+
+          /* Find the # of assets we're loading */
+          var assetsTotal = Q._keys(assetObj).length,
+              assetsRemaining = assetsTotal;
+
+          /* Closure'd per-asset callback gets called */
+          /* each time an asset is successfully loaded */
+          var loadedCallback = function(key,obj,force) {
+            if(errors) { return; }
+
+            console.log('Key:' + key);
+            console.log(Q.assets[key] != false);
+            
+            // Prevent double callbacks (I'm looking at you Firefox, canplaythrough
+            if(!Q.assets[key]||force) {
+
+              /* Add the object to our asset list */
+              Q.assets[key] = obj;
+
+              /* We've got one less asset to load */
+              assetsRemaining--;
+              
+              /* Update our progress if we have it */
+              if(progressCallback) {
+                 progressCallback(assetsTotal - assetsRemaining,assetsTotal);
+              }
+            }
+
+            /* If we're out of assets, call our full callback */
+            /* if there is one */
+            if(assetsRemaining === 0 && callback) {              
+              /* if we haven't set up our canvas element yet, */
+              /* assume we're using a canvas with id 'quintus' */
+              callback.apply(Q);
+            }
+          };
+
+          /* Now actually load each asset */
+          Q._each(assetObj,function(itm,key) {
+            /* Determine the type of the asset */
+            var assetType = Q.assetType(itm);
+
+            /* If we already have the asset loaded, */
+            /* don't load it again */
+            //if(Q.assets[key] && !options.reload) {
+            if(Q.assets[key] && !options.reload) {
+              loadedCallback(key,Q.assets[key],true);
+            } else {
+              /* Call the appropriate loader function */
+              /* passing in our per-asset callback */
+              /* Dropping our asset by name into Q.assets */
+              console.log("calling again the asset load function for loadAsset" + assetType);
+              Q["loadAsset" + assetType](key,itm,
+                                         function(key,obj,force) {
+                                           force = force || options.reload;
+                                           loadedCallback(key, obj, force);     
+                                         },
+                                         function() { errorCallback(itm); });
+            }
+          });
+
+        };    
+    
+    
       Q.loadAssetLevel = function(key,src,callback,errorCallback) {
           var fileParts = src.split("."), fileName = fileParts[0];
-          console.log('Loading: ');
-          console.log(key);
-          console.log(src);
           Q.loadAssetOther(key, "/collectionapi/levels/" + fileName, function(key, val) {
             var obj = JSON.parse(val)[0];
             var board = obj.board;
@@ -190,14 +287,14 @@ _.extend(Template.home, {
               console.log("Error getting level functions:");
               console.log(ex);
             }
-            callback(Q.assets[key]);
+            callback(key, Q.assets[key]);
           }, errorCallback);
       };
       Q.assetTypes.lvl = 'Level'; 
     
       Q.loadAssetSprite = function(key,src,callback,errorCallback) {
         var img = new Image();
-        img.onload = function() {  callback(key,img); };
+        img.onload = function() { callback(key,img); };
         img.onerror = errorCallback;
         img.src = Q.assetUrl("levelSprites/", src);
       };
@@ -205,7 +302,7 @@ _.extend(Template.home, {
     
       Q.loadAssetTile = function(key,src,callback,errorCallback) {
         var img = new Image();
-        img.onload = function() {  callback(key,img); };
+        img.onload = function() { callback(key,img); };
         img.onerror = errorCallback;
         img.src = Q.assetUrl("levelTiles/", src);
       };
@@ -555,8 +652,9 @@ _.extend(Template.home, {
           }
         }
       });
-
-      Q.load("sprites.json, tiles.png, gem1.wav, coin1.wav, shot.json, basicShot.png",  function() {
+      
+      Q.load("sprites.json, tiles.png, shot.json, basicShot.png",  function() {
+      //Q.load("sprites.json, tiles.png, gem1.wav, coin1.wav, shot.json, basicShot.png",  function() {
         console.log("Loaded basic resources...")
       });        
   }  
@@ -574,12 +672,13 @@ _.extend(Template.level, {
       var levelId = this._id;
       levelMapCreate(levelId);
       Q.load(levelId + ".spr, " + levelId + ".lvl, " + levelId + ".til", function() {
+        console.log("Called after all loaded...");
         Q.sheet("tiles", levelId + ".til", { tileW: 32, tileH: 32});        
         Q.compileSheets(levelId + ".spr","sprites.json");
         Q.compileSheets("basicShot.png","shot.json");        
         Q.stageScene(levelId);
         $("#towermanGame").focus();
-      });
+      }, {reload:true});        
     } 
   }
 })
