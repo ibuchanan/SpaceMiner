869a2bb4a10bba279db2f472639fc2ad27103eef
joshgough <josh.gough@versionone.com>
2014-10-25 15:49:17 +0000

    #11 Added title for step and moved pager up

--- server/server.js
+++ server/server.js
@@ -69,12 +69,14 @@ function createLessonsDefault() {
       error: message
     };
   }
-  function step(code, expectation, description) {
+  function step(code, title, id, expectation, description) {
     step.index++;
     return {
+      id: id,
       code: code,
+      title: title,
+      expectation: expectation,      
       description: description,
-      expectation: expectation,
       current: step.index === 0,
       attempted: false,
       index: step.index
@@ -86,11 +88,11 @@ function createLessonsDefault() {
     name: 'variables',
     title: 'Make your game remember things with variables',
     steps: [
-      step('faveColor', error('ReferenceError: faveColor is not defined'), 'You get this error because you first need to tell your computer to recognize <code>faveColor</code> as a <b>variable</b>. A variable lets you store information in the computer\'s RAM. This is easy to do. To learn how, click next...'),
-      step('var faveColor', value(undefined), 'You just used your first JavaScript language keyword, <code>var</code> to tell the computer to reserve a place in memory named <code>faveColor</code>, but right now it is like an emoty brain cell waiting to be filled with information. The response <code>undefined</code> is a type of <b>value</b> that represents a variable that does not have any other assigned value. To get the computer to remember your favorite color, you have to learn about another type of value, and then about <b>variable assignment</b>. Keep going...'),
-      step('red', error('ReferenceError: red is not defined'), 'An error again? The reason is the same as before. The computer does not recognize <b>red</b> as defined variable. But, there is a simple way to use that color name, and any other, in a way that the computer recognizes as a <b>string</b> type...'),
-      step('"red\"', value('"red"'), 'Now you\'re getting somewhere! When the console simply echoes <b>"red"</b> back to you it is telling you that you sent it a value that it could process. In this case, because we surrounded the three characters <b>red</b> with a pair of <b>"</b> characters, the computer recognizes it as a <b>string</b> type. There are several other types of values you can type in that you will learn about later, but try typing each of the following, but do not surroundi them by " charactersjust to get a preview: <ul><li>44</li><li>1.5</li><li>true</li><li>false</li>. Let\'s keep going for now...'),
-      step('faveColor = "red"', value('"red"'), 'Now you have <i>assigned</i> the <b>value</b> of <i>"red"</i> into the <b>variable</b> named <i>faveColor</i>! This is a big step in learning how to code. You really cannot do anything else without mastering this step, so good job! At this point, your computer will forever remember "red" inside of the variable faveColor until you reassign the value, close this page, or leave your computer on long enough that it runs out of power and shuts down! TODO more info')
+      step('faveColor', 'Your first line of code, I do declare', 'undeclared-variable', error('ReferenceError: faveColor is not defined'), 'You get this error because you first need to tell your computer to recognize <code>faveColor</code> as a <b>variable</b>. A variable lets you store information in the computer\'s RAM. This is easy to do. To learn how, click next...'),
+      step('var faveColor', 'Use the var keyword to declare a variable in memory', 'declare-variable', value(undefined), 'You just used your first JavaScript language keyword, <code>var</code> to tell the computer to reserve a place in memory named <code>faveColor</code>, but right now it is like an emoty brain cell waiting to be filled with information. The response <code>undefined</code> is a type of <b>value</b> that represents a variable that does not have any other assigned value. To get the computer to remember your favorite color, you have to learn about another type of value, and then about <b>variable assignment</b>. Keep going...'),
+      step('red', 'Another undeclared variable error message from the interpreter', 'undeclared-variable-red', error('ReferenceError: red is not defined'), 'An error again? The reason is the same as before. The computer does not recognize <b>red</b> as defined variable. But, there is a simple way to use that color name, and any other, in a way that the computer recognizes as a <b>string</b> type...'),
+      step('"red\"', 'Type a string value using double-quotes around text', 'string-variable', value('"red"'), 'Now you\'re getting somewhere! When the console simply echoes <b>"red"</b> back to you it is telling you that you sent it a value that it could process. In this case, because we surrounded the three characters <b>red</b> with a pair of <b>"</b> characters, the computer recognizes it as a <b>string</b> type. There are several other types of values you can type in that you will learn about later, but try typing each of the following, but do not surroundi them by " charactersjust to get a preview:  <p><ul><li>44</li><li>1.5</li><li>true</li><li>false</li></ul></p>.<p>Let\'s keep going for now...</p>'),
+      step('faveColor = "red"', 'Assign a string value into the faveColor variable to make it remember', 'assign-string-value', value('"red"'), 'Now you have <i>assigned</i> the <b>value</b> of <i>"red"</i> into the <b>variable</b> named <i>faveColor</i>! This is a big step in learning how to code. You really cannot do anything else without mastering this step, so good job! At this point, your computer will forever remember "red" inside of the variable faveColor until you reassign the value, close this page, or leave your computer on long enough that it runs out of power and shuts down! TODO more info')
       /*
    Next: <code class='label label-primary'>faveColor</code>
     <br />
